549c1dca50063f948dc468ed4a939423983f6ec4
shaon28 <67582213+SHAON1028@users.noreply.github.com>
2023-03-16 00:03:23 +0600

    product card added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { CardImg } from 'react-bootstrap';
+import React from "react";
+import { Card, CardImg } from "react-bootstrap";
+import Rating from "./Rating";
 
-const Product = ({product}) => {
-    // console.log(product)
-    return (
-        <div  className='py-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
-                <CardImg  src={product.image} variant='top'/>
-            </a>
-        </div>
-    );
+const Product = ({ product }) => {
+  // console.log(product)
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <CardImg src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a className="text-decoration-none" href={`/product/${product._id}`}>
+          <Card.Title as="div" >
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <div className="py-1">
+           <Rating rating={product.rating} reviews={`${product.numReviews} reviews`} color = {'#f8e825'}></Rating>
+          </div>
+        </Card.Text>
+        <Card.Text className="py-2" as="h3">${product.price} </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
